2703b749f58ebc83006cc4bd6846ce82fc183a35
Koji AGAWA <atty303@gmail.com>
2025-07-12 10:47:20 +0900

    feat: add infinitas

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { Command } from "@cliffy/command";
 import { gitadoraCommand } from "./games/gitadora.ts";
+import { infinitasCommand } from "./games/infinitas.ts";
 
 await new Command()
   .name("konaste")
   .command("gitadora", gitadoraCommand)
+  .command("infinitas", infinitasCommand)
   .parse();
